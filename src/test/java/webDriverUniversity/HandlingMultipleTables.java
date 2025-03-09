3295e83698760d6046aac2082fb7e05e2e7d1192
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-09 10:41:49 +0530

    Packages Created

---
+++ src/test/java/webDriverUniversity/HandlingMultipleTables.java
@@ -0,0 +1,10 @@
+package webDriverUniversity;
+
+public class HandlingMultipleTables {
+	
+	/* Test Cases
+	 * 1. Read Two tables data into Single Table and print in console.
+	 * 2. Print the names who are below age 70
+	 * 3. Print First names starts with letter 'J'.
+	 **/
+}
---
+++ src/test/java/webDriverUniversity/HandlingNormalTable.java
@@ -0,0 +1,10 @@
+package webDriverUniversity;
+
+public class HandlingNormalTable {
+
+	/* Test Cases
+	 * 1. Read data from specific row and column (ex: 5th row and 1st col)
+	 * 2. Read the data form all the rows and columns
+	 * 3. Print last name of Larry
+	 * */
+}
