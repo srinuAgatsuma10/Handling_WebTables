d7d40161f562bb39969f0be082b42afb528db969
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-03-14 18:55:11 +0530

    Dynamiv Web Tables

---
+++ src/test/java/automationBlogSpot/HandlingDynamicTable.java
@@ -0,0 +1,27 @@
+package automationBlogSpot;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.*;
+
+public class HandlingDynamicTable {
+	
+	WebDriver driver;
+
+	@BeforeClass
+	public void setUp() {
+		driver = new ChromeDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.manage().window().minimize();
+	}
+	
+	@AfterClass
+	public void tearDown() {
+		driver.quit();
+	}
+}
