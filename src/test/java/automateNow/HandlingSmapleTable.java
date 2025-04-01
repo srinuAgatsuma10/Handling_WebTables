e8abfebdaa23063a84887b250934ef28e5b710b6
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-01 12:05:50 +0530

    First Test Case

--- src/test/java/automateNow/HandlingSmapleTable.java
+++ src/test/java/automateNow/HandlingSmapleTable.java
@@ -1,8 +1,11 @@
 package automateNow;
 
 import java.time.Duration;
+import java.util.List;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -23,13 +26,16 @@ public class HandlingSmapleTable {
 		driver = new ChromeDriver();
 		driver.manage().deleteAllCookies();
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-		driver.get("https://testautomationpractice.blogspot.com/");
+		driver.get("https://practice-automation.com/tables/");
 		driver.manage().window().minimize();
 	}
 	
 	@Test
 	public void itemsInTable() {
-		
+		List<WebElement> items = driver.findElements(By.xpath("//figure[@class='wp-block-table']//table//tbody//tr//td[1]"));
+		for(WebElement item : items) {
+			System.out.println(item.getText());
+		}
 	}
 	
 	@Test
