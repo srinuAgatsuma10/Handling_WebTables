86b4968b27947a4c8fca48848983ff08781bd809
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-02 21:50:03 +0530

    Second TestCase update

--- src/test/java/automateNow/HandlingPaginationTable.java
+++ src/test/java/automateNow/HandlingPaginationTable.java
@@ -8,7 +8,6 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.support.ui.Select;
-import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -17,12 +16,15 @@ public class HandlingPaginationTable {
 
 	/*Test Cases
 	 * 1) Print all countries.
-	 * 2) Print population whose country name starts with 'B'.
+	 * 2) Print population of whose country name starts with 'B'.
 	 * 3) Print sum of total population of all countries in the table.
 	 */
 	
+	
+	/* In this webpage the page navigator of pagination table is not working may be it's bug. So I've followed
+	 Dropdown approach to read values in single time.*/
+	
 	WebDriver driver;
-	WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(10));
 
 	@BeforeClass
 	public void setUp() {
@@ -48,6 +50,20 @@ public class HandlingPaginationTable {
 
 	@Test
 	public void contriesWithB() {
+		// Click On Drop down
+		WebElement myDrp = driver.findElement(By.xpath("//select[@id='dt-length-0']"));
+		Select st = new Select(myDrp);
+		st.selectByVisibleText("25");
+
+		List<WebElement> countries = driver.findElements(By.xpath("//table[@id='tablepress-1']//tbody//tr//td[2]"));
+		for (int i = 0; i < countries.size(); i++) {
+			String nation = countries.get(i).getText();
+			if (nation.startsWith("B")) {
+				String population = driver
+						.findElement(By.xpath("//table[@id='tablepress-1']//tbody//tr[" + i + "]//td[3]")).getText();
+				System.out.println(nation + "\t" + population);
+			}
+		}
 
 	}
 
