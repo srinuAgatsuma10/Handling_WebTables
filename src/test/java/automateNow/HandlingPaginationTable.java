e4b09310fb0d89a41e2c35df22aecfb95ebb8e4b
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-02 19:05:57 +0530

    First Test Case Update

--- src/test/java/automateNow/HandlingPaginationTable.java
+++ src/test/java/automateNow/HandlingPaginationTable.java
@@ -7,15 +7,14 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.support.ui.ExpectedCondition;
-import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
 import org.openqa.selenium.support.ui.WebDriverWait;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 
 public class HandlingPaginationTable {
-	
+
 	/*Test Cases
 	 * 1) Print all countries.
 	 * 2) Print population whose country name starts with 'B'.
@@ -24,7 +23,7 @@ public class HandlingPaginationTable {
 	
 	WebDriver driver;
 	WebDriverWait mywait = new WebDriverWait(driver, Duration.ofSeconds(10));
-	
+
 	@BeforeClass
 	public void setUp() {
 		driver = new ChromeDriver();
@@ -33,35 +32,33 @@ public class HandlingPaginationTable {
 		driver.get("https://practice-automation.com/tables/");
 		driver.manage().window().maximize();
 	}
-	
+
 	@Test
 	public void allCountries() {
-		for(int i =1;i<3;i++) {
-			if(i>1) {
-				WebElement next =driver.findElement(By.xpath("//nav[@aria-label='pagination']//button[@data-dt-idx="+i+"]"));
-				mywait.until(ExpectedConditions.elementToBeClickable(next)).click();
-			}
-			
-			List<WebElement> countries = driver.findElements(By.xpath("//table[@id='tablepress-1']//tbody//tr//td[2]"));
-			for(WebElement country : countries) {
-				System.out.println(country.getText());
-			}
+		// Click On Drop down
+		WebElement myDrp = driver.findElement(By.xpath("//select[@id='dt-length-0']"));
+		Select st = new Select(myDrp);
+		st.selectByVisibleText("25");
+
+		List<WebElement> countries = driver.findElements(By.xpath("//table[@id='tablepress-1']//tbody//tr//td[2]"));
+		for (WebElement country : countries) {
+			System.out.println(country.getText());
 		}
 	}
-	
+
 	@Test
 	public void contriesWithB() {
-		
+
 	}
-	
+
 	@Test
 	public void sumOfTotalPopulation() {
-		
+
 	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
 	}
-	
+
 }
