9355e2cd763fd50342f250d056cf9e6ffd402d06
Giridhara Srinivas <ggsrinivascm035@gmail.com>
2025-04-03 08:29:39 +0530

    automationBlogSpot Dynamic table First Test Case

--- src/test/java/automationBlogSpot/HandlingDynamicTable.java
+++ src/test/java/automationBlogSpot/HandlingDynamicTable.java
@@ -4,19 +4,15 @@ import java.time.Duration;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
-import org.testng.annotations.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class HandlingDynamicTable {
-	
-	/* Validate these four test cases/
-	 * CPU load of Chrome process is equal to value in table
-	 * Memory Size of Firefox process is equal to value in table
-	 * Network speed of Chrome process is equal to value in table
-	 * Disk space of Firefox process is equal to value in table
-	 */
-	
+
 	WebDriver driver;
 
 	@BeforeClass
@@ -27,53 +23,69 @@ public class HandlingDynamicTable {
 		driver.get("https://testautomationpractice.blogspot.com/");
 		driver.manage().window().minimize();
 	}
-	
-	@Test
+
+	@Test(description = "CPU load of Chrome process")
 	public void chromCPULoad() {
-		//CPU load of Chrome process
-		String tableValue = driver.findElement(By.xpath("//table[@id=\"taskTable\"]//tbody//tr[1]//td[5]")).getText();
-		String descValue = driver.findElement(By.xpath("")).getText();
-		
+		int columnCount = driver.findElements(By.xpath("//table[@id='taskTable']//thead//tr//th")).size();
+		int rowCount = driver.findElements(By.xpath("//table[@id='taskTable']//tbody//tr")).size();
+
+		int cpuColumnIndex = -1;
+		String descValue = driver.findElement(By.xpath("//strong[@class='chrome-cpu']")).getText();
+
+		// Find the column index for "CPU (%)"
+		for (int i = 1; i <= columnCount; i++) {
+			String header = driver.findElement(By.xpath("//table[@id='taskTable']//thead//tr//th[" + i + "]"))
+					.getText();
+			if (header.equals("CPU (%)")) {
+				cpuColumnIndex = i;
+				break;
+			}
+		}
+
+		// If "CPU (%)" column is not found, exit
+		if (cpuColumnIndex == -1) {
+			System.out.println("CPU (%) column not found.");
+			return;
+		}
+
+		// Find the row where "Chrome" is located
+		for (int j = 1; j <= rowCount; j++) {
+			String browserName = driver.findElement(By.xpath("//table[@id='taskTable']//tbody//tr[" + j + "]//td[1]"))
+					.getText();
+
+			if (browserName.equals("Chrome")) {
+				// Get the CPU usage value from the correct row and column
+				String cpuUsage = driver
+						.findElement(
+								By.xpath("//table[@id='taskTable']//tbody//tr[" + j + "]//td[" + cpuColumnIndex + "]"))
+						.getText();
+				if (cpuUsage.equals(descValue)) {
+					System.out.println("Chrome CPU Usage: " + cpuUsage);
+					Assert.assertTrue(true);
+				}
+			} 
+		}
+
 	}
-	
+
 	@Test
 	public void firefoxMemorySize() {
 		String tableValue = driver.findElement(By.xpath("")).getText();
 		String descValue = driver.findElement(By.xpath("")).getText();
 	}
-	
+
 	@Test
 	public void chromeNetworkSpeed() {
 		String tableValue = driver.findElement(By.xpath("")).getText();
 		String descValue = driver.findElement(By.xpath("")).getText();
 	}
-	
+
 	@Test
 	public void firefoxDiscSpace() {
 		String tableValue = driver.findElement(By.xpath("")).getText();
-		String descValue = driver.findElement(By.xpath("//strong[@class='chrome-cpu']")).getText();
+
 	}
-	
-	@Test
-	public void testMethod() {
-		int i;
-		int j;
-		String descValue = driver.findElement(By.xpath("//strong[@class='chrome-cpu']")).getText();
-		for( i =1;i<=5;i++) {
-			String headText = driver.findElement(By.xpath("//table[@id=\"taskTable\"]//thead//tr//th["+i+"]")).getText();
-			String name = driver.findElement(By.xpath("//table[@id='taskTable']//tbody//tr[1]//td["+i+"]")).getText();
-			if(headText.equals("CPU (%)") && name.equals("Chrome")) {
-				for(j=1;j<=5;j++) {
-					String cellData = driver.findElement(By.xpath("//table[@id='taskTable']//tbody//tr["+j+"]//td["+i+"]")).getText();
-					System.out.println(cellData);
-					if(cellData.equals(descValue)) {
-						Assert.assertTrue(false);
-					}
-				}
-			}
-		}
-	}
-	
+
 	@AfterClass
 	public void tearDown() {
 		driver.quit();
